f89924b7b3285a94606fe6a51a1392e9cc04a4e3
ryouta170507-code <ryouta170507@gmail.com>
2026-06-24 14:16:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from fastapi.staticfiles import StaticFiles  
-from livekit_api import AccessToken, VideoGrant
+from fastapi.staticfiles import StaticFiles
+from livekit import AccessToken, VideoGrant
 
 app = FastAPI()
 
@@ -9,7 +9,6 @@ API_KEY = "API5JKYXvyDCxvN"
 API_SECRET = "7XcGx9xagBJK0fgbjT1Z1kxTAkrulZZf24pCvYnYOSa"
 ROOM_NAME = "team-room"
 
-# 静的ファイル
 app.mount("/css", StaticFiles(directory="css"), name="css")
 app.mount("/js", StaticFiles(directory="js"), name="js")
 
@@ -17,34 +16,10 @@ app.mount("/js", StaticFiles(directory="js"), name="js")
 def read_index():
     return FileResponse("index.html")
 
-connected_clients: list[WebSocket] = []
-
-@app.websocket("/ws/signal")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    connected_clients.append(websocket)
-    print(f"【ログ】新しいクライアントが接続しました。現在の接続数: {len(connected_clients)}")
-
-    try:
-        while True:
-            data = await websocket.receive_text()
-            print(f"【ログ】受信したデータ: {data}")
-
-            for client in connected_clients:
-                if client != websocket:
-                    await client.send_text(data)
-
-    except WebSocketDisconnect:
-        connected_clients.remove(websocket)
-        print(f"【ログ】クライアントが切断しました。現在の接続数: {len(connected_clients)}")
-
-
 @app.get("/token")
 def get_token(identity: str):
     grant = VideoGrant(room_join=True, room=ROOM_NAME)
-
     token = AccessToken(API_KEY, API_SECRET)
     token.identity = identity
     token.add_grant(grant)
-
     return {"token": token.to_jwt()}
